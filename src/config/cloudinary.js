2dcbfdcd9e788017a57143ed11a73d3a05f8d63f
Abeni <abenezeryirgalem0@gmail.com>
2026-04-09 17:03:06 +0300

    Fix import formatting in cloudinary.js

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary';
+﻿import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,3 +10,4 @@ cloudinary.config({
 });
 
 export default cloudinary;
+
