88e197516241bd59b58885d9997a5ee7c9b41a43
Abeni <abenezeryirgalem0@gmail.com>
2026-04-09 17:03:31 +0300

    Fix import formatting in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+﻿import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
@@ -14,3 +14,4 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
